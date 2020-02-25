3c7bcddb8de4a5e9d34830cad03f57b22f2ca62a
Katelyn <katelyn.offerdahl@duke.edu>
2020-02-25 13:58:32 -0500

    version with 2 possible methods for doing attenuation.

--- MIP_viewer.m
+++ MIP_viewer.m
@@ -4,10 +4,10 @@ clear all
 
 num_angles = 50;
 depth_weighting = 0;
-
+d = 3;
 %% Load Data
 
-data = 'phantompet15sec.fl'
+data = 'phantompet3min.fl'
 [fID, err] = fopen(data);
 
 read_data = fread(fID, 'float32');
@@ -37,10 +37,28 @@ for slice_ix = 1:num_slices
         i1 = round((size(rot_data, 1) - frame_size)/2);
         ind1 = i1+1:i1+frame_size;
         crop_rot_data = rot_data(ind1, ind1);
-        
+        crop_rot_data_orig = crop_rot_data;
+        %attenuation = (size(crop_rot_data, 2):-1:1).*exp(-d);
+        %attenuation = repmat(attenuation, 128, 1)';
+        %crop_rot_data = crop_rot_data .*attenuation;
         [M,I] = max(crop_rot_data,[],2);
-        data_out(:,slice_ix,angle_ix) = M;
-        max_locs(:,slice_ix,angle_ix) = I;
+        data_out_orig(:,slice_ix,angle_ix) = M;
+        max_locs_orig(:,slice_ix,angle_ix) = I;
+        
+        
+        attenuation = I .*exp(-d);
+        M_attenuated = M .* attenuation;
+        data_out_attn2(:,slice_ix,angle_ix) = M_attenuated;
+        max_locs_attn2(:,slice_ix,angle_ix) = I;
+        
+        
+        crop_rot_data = rot_data(ind1, ind1);
+        attenuation = (size(crop_rot_data, 2):-1:1).*exp(-d);
+        attenuation = repmat(attenuation, 128, 1)';
+        crop_rot_data = crop_rot_data .*attenuation;
+                [M,I] = max(crop_rot_data,[],2);
+        data_out_attn1(:,slice_ix,angle_ix) = M;
+        max_locs_attn1(:,slice_ix,angle_ix) = I;
      end
     disp(slice_ix)
 end
@@ -48,8 +66,23 @@ end
 %% Visualize Rotation
 
 for i = 1:num_angles
-    imagesc(squeeze(data_out(:,:,i)))
+    subplot(131)
+    imagesc(squeeze(data_out_orig(:,:,i))')
+    colorbar
+    %caxis([0 1e7])
+            caxis([0 5e4])
+
+                subplot(132)
+        imagesc(squeeze(data_out_attn1(:,:,i))')
+    colorbar
+        caxis([0 0.1e6])
+            
     colormap gray
+    subplot(133)
+        imagesc(squeeze(data_out_attn2(:,:,i))')
+    colorbar
+        caxis([0 0.1e6])
+
     pause(0.1)
 end
 
@@ -69,7 +102,7 @@ CT_data = reshape(read_data_CT, frame_size, frame_size, []);
 %downsample CT
 CT_data = CT_data(1:4:frame_size, 1:4:frame_size, :);
 CT_data = double(CT_data); % for the filtering
-
+CT_data_orig = CT_data;
 %minimum filter to remove bed artifacts
 %fun = @(x) min(x(:));
 for i = 1:size(CT_data, 3)
