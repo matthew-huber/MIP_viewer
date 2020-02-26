456c3a17c20571a85b75746ff2319c30996d3fee
Katelyn <katelyn.offerdahl@duke.edu>
2020-02-26 11:53:37 -0500

    Adds attenuation starting from beginning of body.

--- MIP_viewer.m
+++ MIP_viewer.m
@@ -4,10 +4,10 @@ clear all
 
 num_angles = 50;
 depth_weighting = 0;
-d = 3;
+d = 3; % attenuation factor
 %% Load Data
 
-data = 'phantompet3min.fl'
+data = 'petimg.fl'
 [fID, err] = fopen(data);
 
 read_data = fread(fID, 'float32');
@@ -27,113 +27,92 @@ data_out = zeros(128,num_slices,num_angles);
 max_locs = zeros(128,num_slices,num_angles);
 
 
+%% Read in CT Data
+CT_data = 'ctimg.sh';
+[fID, err] = fopen(CT_data);
+
+read_data_CT = fread(fID, 'int16');
+fclose(fID);
+
+CT_frame_size=512;
+CT_data = reshape(read_data_CT, CT_frame_size, CT_frame_size, []);
+
+%downsample CT
+%CT_data = CT_data(4:4:CT_frame_size, 4:4:CT_frame_size, :);
+CT_data = imresize(CT_data, [frame_size, frame_size]);
+CT_data = double(CT_data); % for the filtering
+CT_data_orig = CT_data;
+pd_size = 4;
+CT_data = padarray(CT_data, [0,pd_size, 0], min(CT_data(:)), 'both');
+
+for i = 1:size(CT_data, 3) % add blut filter
+    CT_data(:, :, i) = colfilt(CT_data(:, :, i), [2,8],'sliding', @median);
+end
+CT_data = CT_data(:, (pd_size/2):(128+pd_size/2), :); % restore to normal size after padding
+
+CT_data = CT_data - min(CT_data(:));
+
+body_start_thresh = 2924;
+min_thresh_len = 5; % number of consecutive samples above thresh to be considered part of body
 %%
 
 for slice_ix = 1:num_slices
+    %CT_frame = CT_data(:, :, slice_ix);
     for angle_ix = 1:num_angles
         angle = angles(angle_ix);
         rot_data = imrotate(data_in(:,:,slice_ix),angle);
-        
+        CT_rot_data = imrotate(CT_data(:, :, slice_ix), angle);
         i1 = round((size(rot_data, 1) - frame_size)/2);
         ind1 = i1+1:i1+frame_size;
         crop_rot_data = rot_data(ind1, ind1);
+        CT_rot_data = CT_rot_data(ind1, ind1);
+        body_start_inx = zeros(1, size(CT_rot_data, 2));
+        for j = 1:size(CT_rot_data, 2)
+            CT_line = CT_rot_data(:, j);
+            body_start_inx(j) = thresh_region(CT_line, body_start_thresh, min_thresh_len);
+        end
+        
+        
         crop_rot_data_orig = crop_rot_data;
-        %attenuation = (size(crop_rot_data, 2):-1:1).*exp(-d);
-        %attenuation = repmat(attenuation, 128, 1)';
-        %crop_rot_data = crop_rot_data .*attenuation;
-        [M,I] = max(crop_rot_data,[],2);
+        
+        [M,I] = max(crop_rot_data,[],1);
         data_out_orig(:,slice_ix,angle_ix) = M;
         max_locs_orig(:,slice_ix,angle_ix) = I;
         
-        
-        attenuation = I .*exp(-d);
+        dist_to_max = I - body_start_inx;
+        dist_to_max(dist_to_max<0)=0;
+        attenuation = dist_to_max .*exp(-d);
         M_attenuated = M .* attenuation;
         data_out_attn2(:,slice_ix,angle_ix) = M_attenuated;
         max_locs_attn2(:,slice_ix,angle_ix) = I;
         
-        
-        crop_rot_data = rot_data(ind1, ind1);
-        attenuation = (size(crop_rot_data, 2):-1:1).*exp(-d);
-        attenuation = repmat(attenuation, 128, 1)';
-        crop_rot_data = crop_rot_data .*attenuation;
-                [M,I] = max(crop_rot_data,[],2);
-        data_out_attn1(:,slice_ix,angle_ix) = M;
-        max_locs_attn1(:,slice_ix,angle_ix) = I;
-     end
+        %         Alternative Attenuation method
+        %         crop_rot_data = rot_data(ind1, ind1);
+        %         attenuation = (size(crop_rot_data, 2):-1:1).*exp(-d);
+        %         attenuation = repmat(attenuation, 128, 1)';
+        %         crop_rot_data = crop_rot_data .*attenuation;
+        %                 [M,I] = max(crop_rot_data,[],2);
+        %         data_out_attn1(:,slice_ix,angle_ix) = M;
+        %         max_locs_attn1(:,slice_ix,angle_ix) = I;
+    end
     disp(slice_ix)
 end
 
 %% Visualize Rotation
 
 for i = 1:num_angles
-    subplot(131)
+    subplot(121)
     imagesc(squeeze(data_out_orig(:,:,i))')
     colorbar
-    %caxis([0 1e7])
-            caxis([0 5e4])
-
-                subplot(132)
-        imagesc(squeeze(data_out_attn1(:,:,i))')
-    colorbar
-        caxis([0 0.1e6])
-            
+    caxis([0 5e4])
+    title('Original')
     colormap gray
-    subplot(133)
-        imagesc(squeeze(data_out_attn2(:,:,i))')
+    subplot(122)
+    imagesc(squeeze(data_out_attn2(:,:,i))')
     colorbar
-        caxis([0 0.1e6])
-
+    caxis([0 0.1e6])
+    title('Attenuated')
     pause(0.1)
 end
-
-
-%% Load CT Data
-
-CT_data = 'phantomct.sh'
-[fID, err] = fopen(CT_data);
-
-read_data_CT = fread(fID, 'int16');
-fclose(fID);
-
-%% Reshape data and create matrices for output
-frame_size=512;
-CT_data = reshape(read_data_CT, frame_size, frame_size, []);
-
-%downsample CT
-CT_data = CT_data(1:4:frame_size, 1:4:frame_size, :);
-CT_data = double(CT_data); % for the filtering
-CT_data_orig = CT_data;
-%minimum filter to remove bed artifacts
-%fun = @(x) min(x(:));
-for i = 1:size(CT_data, 3)
-    CT_data_filt(:, :, i) = colfilt(CT_data(:, :, i), [6,6],'sliding', @min);
-        CT_data_filt(:, :, i) = colfilt(CT_data_filt(:, :, i), [2,2],'sliding', @mean);
-
-end
-
-
-%% Visualize
-for i = 1:size(CT_data, 3)
-    subplot(131)
-    imagesc(CT_data(:, :, i))
-    colorbar
-    caxis([-1200 0])
-    
-    subplot(132)
-    imagesc(CT_data_orig(:, :, i));
-    colorbar
-    caxis([-1200 0])
-    
-    subplot(133)
-    imagesc(data_in(:, :,i))
-    
-        colorbar
-    pause(0.2)
-end
-
-% hold body start position here, compare with max_locs to find distance for
-% attenuation
-body_start = zeros(128,num_slices,num_angles);
-
 
 
---
+++ thresh_region.m
@@ -0,0 +1,29 @@
+function [region_start] = thresh_region(CT_line,thresh, N)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+BV = CT_line>thresh;
+delta_line = diff(BV);
+start_inx = find(delta_line==1);
+end_inx = find(delta_line==-1);
+
+if length(start_inx) > length(end_inx)
+    start_inx = start_inx(1:end-1);
+end
+if length(end_inx) > length(start_inx)
+    start_inx = [1 start_inx'];
+end
+
+    
+
+region_lengths = end_inx - start_inx;
+
+region_num = find(region_lengths>N, 1, 'first');
+
+region_start = start_inx(region_num);
+if isempty(region_start)
+    region_start = 1;
+end
+
+
+end
+
